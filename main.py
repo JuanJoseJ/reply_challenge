68fa9bc5804ea1526234762e7868bb642b07f1ec
JuanJoseJ <s301352@polito.studenti.it>
2023-03-09 11:41:54 -0500

    add snake

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+class Snake:
+    def __init__(self, length):
+        self.length = length
+        segments = np.zeros((self.length, 2))
+        nextStep = np.array((4, 2)) 
+        
+
 def main():
     
     return
